74ac674d2a983a0feba66122666f71f412aaff53
taejin <taejin5713@gmail.com>
2023-12-01 11:59:19 +0900

    asda

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,39 @@
 "use client"
 
+
+import {  LoadScript } from '@react-google-maps/api';
 import { useLoadScript, GoogleMap, MarkerF, InfoWindowF } from '@react-google-maps/api';
 import type { NextPage } from 'next';
 import { use, useEffect, useMemo, useState } from 'react';
 import { save } from './actions/save';
 import { getMarkers } from './actions/get-markers';
 
-const Home: NextPage = () => {
-  const [newMarker, setNewMarker] = useState<{ lat: any, lng: any } | null>(null)
-  const [markers, setMarkers] = useState<{ lat: any, lng: any, spotifyID: string }[]>([])
+const Map = () => {
+// 작동 안함
+  const [map, setMap] = useState<google.maps.Map | null>(null);
+  
 
-  const [activeMarker, setActiveMarker] = useState<{ lat: any, lng: any } | null>(null);
-  const [center, setCenter] = useState<{ lat: any, lng: any }>({ lat: 37.517235, lng: 127.047325 })
-
+ 
+  return (
+    <LoadScript
+      googleMapsApiKey="AIzaSyDWxqDaMcqcMzuXwrSHa0wO9l2pQ41rLCw"
+      libraries={['geometry', 'places']}
+    >
+      <GoogleMap
+        mapContainerStyle={{ height: '400px', width: '100%' }}
+        zoom={14}
+       
+        onLoad={(Map) => setMap(map)}
+      ></GoogleMap>
+    </LoadScript>
+  );
+  }
+  const Home: NextPage = () => {
+    const [newMarker, setNewMarker] = useState<{ lat: any, lng: any } | null>(null)
+    const [markers, setMarkers] = useState<{ lat: any, lng: any, spotifyID: string }[]>([])
+  
+    const [activeMarker, setActiveMarker] = useState<{ lat: any, lng: any } | null>(null);
+    const [center, setCenter] = useState<{ lat: any, lng: any }>({ lat: 37.517235, lng: 127.047325 })
 
   const onMapClick = (e : any) => {
     setNewMarker({
@@ -21,12 +42,32 @@ const Home: NextPage = () => {
     })
   };
 
+  const [inputVisible, setInputVisible] = useState(false);
+
+  const toggleInput = () => {
+    setInputVisible(true);
+  };
+
+  const submitForm = () => {
+    // Add your form submission logic here
+    console.log("Form submitted!");
+  };
 
 
   useEffect(() => {
     getMarkers().then(res => {
       setMarkers(res)
     })
+
+    
+  navigator.geolocation.getCurrentPosition(
+    (position) => {   
+      const { latitude, longitude } = position.coords;
+ setCenter({  lat: latitude, lng: longitude})
+      
+    },
+
+  );
   }, [])
 
 
@@ -44,6 +85,8 @@ const Home: NextPage = () => {
     []
   );
 
+  
+
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyDWxqDaMcqcMzuXwrSHa0wO9l2pQ41rLCw",
     libraries: libraries as any,
@@ -58,6 +101,9 @@ const Home: NextPage = () => {
 
 
   return (
+
+    
+    
     <form action={async (formData: FormData) => {
       if (newMarker == null) {
         alert('위치를 선택해주세요.')
@@ -85,7 +131,7 @@ const Home: NextPage = () => {
       }
     }}> 
       <GoogleMap
-        center={center}
+        center={center} 
         options={mapOptions}
         zoom={14}
         mapTypeId={google.maps.MapTypeId.ROADMAP}
@@ -99,9 +145,13 @@ const Home: NextPage = () => {
               position={{
                 lat: newMarker.lat,
                 lng: newMarker.lng
-              }} />
+              }} 
+              />
           )
+          
+          
         }
+        
 
         {
           markers && (
@@ -139,11 +189,69 @@ const Home: NextPage = () => {
           )
         }
       </GoogleMap>
+      
 
       <div className='container'>
-        <input type="url" name="link" className='ring  rounded-xl outline-none'></input>
-        <button type='submit'>저장하기</button>
-      </div>
+      <input 
+        type="url"
+        name="link"
+        className="ring rounded-xl outline-none"
+        id="urlInput"
+        style={{
+
+
+          
+          opacity: inputVisible ? 1 : 0,
+          width: inputVisible ? '150px' : '0',
+          padding: '10px',
+          border: '1px solid #ccc',
+          borderRadius: '8px 0 0 8px',
+          outline: 'none',
+          transition: 'opacity 0.5s, width 0.5s',
+          display: 'flex',
+          flexDirection: 'column',// 작동 안함
+          alignItems: 'center',
+          textAlign: 'center',
+          justifyContent: 'center',
+          
+        }}
+      />
+      <img
+        src="https://w7.pngwing.com/pngs/68/239/png-transparent-number-computer-icons-plus-miscellaneous-game-plus.png"
+        alt="plus icon"
+        className="icon"
+        onClick={toggleInput}
+        style={{
+          opacity: inputVisible ? 0 : 1,
+          width: '40px',//아이콘 크기
+          height: '40px',
+          cursor: 'pointer',
+          transition: 'opacity 0.5s',
+         
+        }}
+      />
+      <button
+        type="submit"
+        id="saveButton"
+        onClick={submitForm}
+        style={{
+          opacity: inputVisible ? 1 : 0,
+          width: inputVisible ? '150px' : '0',
+          padding: '10px 15px',
+          backgroundColor: '#4CAF50',
+          color: 'white',
+          border: '1px solid #4CAF50',
+          borderRadius: '8px', // Adjusted to have rounded corners
+          cursor: 'pointer',
+          outline: 'none',
+          transition: 'opacity 0.5s, width 0.5s',
+       
+        }}
+      >
+        저장하기
+      </button>
+    </div>
+
     </form>
   );
 };
